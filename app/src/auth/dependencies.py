2745802bc52c1933e5bd3fb319e2fefef8f8eb1e
aminishereai <giriamin69@gmail.com>
2026-06-17 13:00:25 +0545

    feat: created current users dependency

--- app/src/auth/dependencies.py
+++ app/src/auth/dependencies.py
@@ -1,20 +1,41 @@
 from typing import Annotated
 
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import jwt
 from sqlmodel import select
 
 from app.core.database import SessionDep
 from app.src.auth.models import Token, Users, UsersCreate
-from app.src.auth.services import authenticate_user, create_access_token
+from app.src.auth.services import ALGORITHM, SECRET_KEY, authenticate_user, create_access_token
 
-credentials_exception = lambda name :  HTTPException(
+credentials_exception = lambda name , **kwargs :  HTTPException(
     status_code= status.HTTP_401_UNAUTHORIZED,
-    detail= f"{name} is not authorized"
+    detail= f"{name} is not authorized",
+    **kwargs
 )
 
+oauth2_scheme = OAuth2PasswordBearer("auth/login/")
 
+def get_current_user(token : Annotated[str , Depends(oauth2_scheme)] , session : SessionDep) -> Users:
+    cred_exp = credentials_exception("User" , headers={"WWW-Authenticate": "Bearer"})
+    try : 
+        payload = jwt.decode(token , SECRET_KEY , [ALGORITHM])
+        username = payload.get("sub")
+        statement = select(Users).where(Users.name == username)
+        user = session.exec(statement).one_or_none()
+        # Checks for existing user
+        if not user :
+            raise HTTPException(
+                status_code= status.HTTP_404_NOT_FOUND,
+                detail = f"Username : {username} does not Exists"
+            )
+        
+        return user
 
+    except Exception:
+        raise cred_exp
+    
 
 def create_user(user : UsersCreate , session : SessionDep)-> Users:
     statement = select(Users).where(Users.name == user.name)
@@ -65,3 +86,4 @@ def login_user(
 
 LoginUserDep = Annotated[Users , Depends(login_user) ]
 CreateUserDep = Annotated[Users , Depends(create_user) ]
+CurrentUserDep = Annotated[Users , Depends(get_current_user) ]
