694f1bef64fece9865395d578da021b790ccd41a
aminishereai <giriamin69@gmail.com>
2026-07-02 20:12:54 +0545

    feat: setup the schemas and models of expences

---
+++ app/src/expenses/models.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+from enum import Enum
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+
+class Split(str , Enum):
+    disproportionate = "disproportionate"
+    equal = "equal"
+
+# Tables 
+class Expenses(SQLModel , table=True):
+    id : Optional[int] = Field(index=True , default= None , primary_key=True)
+    user_id : int = Field(foreign_key="users.id", ondelete="CASCADE")
+    group_id : int = Field(foreign_key="groups.id", ondelete="CASCADE")
+    total_amt : Decimal = Field(max_digits=10 , decimal_places=2)
+    split_type : Split
+
+class ExpenseSplit(SQLModel ,table=True) :
+    user_id : int = Field(foreign_key="users.id" , ondelete="CASCADE")
+    expense_id : int = Field(foreign_key="expenses.id" , primary_key=True, ondelete="CASCADE")
+    decided_amt : Decimal = Field(max_digits=10 , decimal_places=2) 
+  
+# Schemas
+class ExpenseIn(SQLModel):
+    group_id : int 
+    total_amt : Decimal = Field(max_digits=10 , decimal_places=2)
+    split_type : Split
+
