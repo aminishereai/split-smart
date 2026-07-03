db078d8404ad27feed887f53c62a031d1d717a3c
aminishereai <giriamin69@gmail.com>
2026-07-03 07:41:02 +0545

    feat: created spliting service function

--- app/src/expenses/services.py
+++ app/src/expenses/services.py
@@ -8,8 +8,9 @@ def splitter(expense: ExpenseIn , user : Users , expense_id : int , n : int):
     total_amt: Decimal = expense.total_amt
     decided_amts: list[Decimal] = [Decimal('0.00')] * n
     payer = user
+
+    assert expense.splits is not None
     if expense.split_type == Split.disproportionate:
-        assert expense.splits is not None
         decided_amts = [ (total_amt * Decimal(x.percentage) / Decimal('100')).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP) for x in expense.splits]
         
     if expense.split_type == Split.equal :
@@ -19,5 +20,10 @@ def splitter(expense: ExpenseIn , user : Users , expense_id : int , n : int):
     round_rem = (total_amt - sum(decided_amts))
     decided_amts[0] = (decided_amts[0] + round_rem).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
 
+    # create expeense split object 
     expense_splited = [ExpenseSplit(user_id = x.user_id , expense_id = expense_id , decided_amt=y) for x , y in zip(expense.splits , decided_amts)]
     
+    # Filters the payer amt altogether
+    expense_splited = [x for x in expense_splited if x.user_id != payer.id]
+
+    return expense_splited
