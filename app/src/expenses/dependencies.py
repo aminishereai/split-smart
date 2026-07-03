b73f604d12ea17e2b25ca79126f080b85019313f
aminishereai <giriamin69@gmail.com>
2026-07-03 08:05:59 +0545

    feat: created spliting service function

---
+++ app/src/expenses/dependencies.py
@@ -0,0 +1,41 @@
+from fastapi import HTTPException , status
+from sqlmodel import Session, select , func
+
+from app.src.expenses.models import ExpenseIn, ExpenseSplit, Expenses
+from app.src.expenses.services import splitter
+from app.src.groups.models import UserGroupJunction
+
+
+def add_expense (expense_data :ExpenseIn , session : Session , user_id : int , group_id : int) ->list[ExpenseSplit]:
+    expense = Expenses(
+        user_id=user_id,
+        **expense_data.model_dump()
+    )
+    session.add(expense)
+    session.commit()
+    session.refresh(expense)
+
+    if not expense.id :
+        raise HTTPException(
+            status_code=status.HTTP_501_NOT_IMPLEMENTED,
+            detail=f"Expense is not created"
+        )
+
+
+
+    statement = (
+        select(func.count())
+        .select_from(UserGroupJunction)
+        .where(UserGroupJunction.group_id == group_id)
+    )
+    
+    n = int(session.exec(statement).one())
+
+    expense_split = splitter(
+        expense=expense_data,
+        user_id=user_id,
+        expense_id=expense.id,
+        n=n,
+    )
+
+    return expense_split
