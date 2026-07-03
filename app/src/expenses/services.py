3acf2350f600e806776d216dfabdbe4074bf47bf
aminishereai <giriamin69@gmail.com>
2026-07-03 07:44:41 +0545

    feat: created spliting service function

--- app/src/expenses/services.py
+++ app/src/expenses/services.py
@@ -1,13 +1,12 @@
 from decimal import Decimal, ROUND_HALF_UP
 
-from app.src.auth.models import Users
 from app.src.expenses.models import ExpenseIn, ExpenseSplit, Split
 
 
-def splitter(expense: ExpenseIn , user : Users , expense_id : int , n : int):
+def splitter(expense: ExpenseIn , user_id : int , expense_id : int , n : int):
     total_amt: Decimal = expense.total_amt
     decided_amts: list[Decimal] = [Decimal('0.00')] * n
-    payer = user
+    payer_id = user_id
 
     assert expense.splits is not None
     if expense.split_type == Split.disproportionate:
@@ -24,6 +23,6 @@ def splitter(expense: ExpenseIn , user : Users , expense_id : int , n : int):
     expense_splited = [ExpenseSplit(user_id = x.user_id , expense_id = expense_id , decided_amt=y) for x , y in zip(expense.splits , decided_amts)]
     
     # Filters the payer amt altogether
-    expense_splited = [x for x in expense_splited if x.user_id != payer.id]
+    expense_splited = [x for x in expense_splited if x.user_id != payer_id]
 
     return expense_splited
